d93bd0566a2b69cd4a2cfee27687e9225c77cdc4
a_morev <a.morev@nari.ru>
2020-02-14 11:35:12 +0300

    empty project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Hello World!");
+		};
+	}
+}
